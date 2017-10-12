b3d0c5dd0db2281dc2fa60fa6321548dd9efa18e
daxian <1369617918@qq.com>
2017-10-12 15:32:08 +0800

    add:添加话术

--- sell_assistant/autobot.py
+++ sell_assistant/autobot.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 bots = {}
-bot = Bot('../cfgs/huarui')
+bot1 = Bot('../cfgs/huarui')
 
 
 @app.route("/goon")
@@ -30,8 +30,13 @@ def reply():
 
 @app.route("/start")
 def start():
-    global bot
-    bot = bot.reset()
+    global bot1
+    bot = ''
+    # todo 添加话术参数，为后来的多种话术进行铺垫
+    trick = request.args.get("trick")
+    if trick == 'huarui':
+        bot = bot1.reset()
+
     session = request.args.get("session")
     if session is None:
         return "authority deny"
