8998705a284a934034e1891f7cac7ba66f6c93b0
wuyingqiang <794870409@qq.com>
2018-01-09 14:53:12 +0800

    变更主动推进

--- sell_assistant/bot_test.py
+++ sell_assistant/bot_test.py
@@ -3,7 +3,6 @@ from bot import Bot
 
 if __name__ == '__main__':
     bot1 = Bot('../cfgs/tricks/huarui')
-    bo2 = Bot('../cfgs/tricks/huarui')
     state, sentence = bot1.start()
     print(state, sentence)
 
@@ -11,8 +10,8 @@ if __name__ == '__main__':
         user_input = input('user input: ')
         if user_input == 'q':
             break
-        state, sentence = bot.answer(user_input)
+        state, sentence = bot1.answer(user_input)
         print(state, sentence)
         if '结束' in state:
-            print(bot.get_label())
+            print(bot1.get_label())
             break
